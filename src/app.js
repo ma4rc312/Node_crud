0f17b460942e5161ed398da980ed384def219091
MarceLucio05 <lucioyurimarcela03@gmail.com>
2023-01-18 07:12:57 -0500

    imagen multer

--- src/app.js
+++ src/app.js
@@ -1,24 +1,54 @@
 import express from "express";
 import projectsRoutes from "./routes/projects.routes.js";
 import tasksRoutes from "./routes/task.routes.js";
-import ImgRoutes from './routes/img.routes.js';
-import morgan from "morgan";
-import path from "path"
+import imgRoutes from "./routes/img.routes.js";
+import multer from "multer";
+import { v4 as uuidv4 } from 'uuid';
 
+import path from "path"
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// middlewares
-app.use(morgan('dev'));
-app.use(express.json());
-
 app.use(projectsRoutes);
 app.use(tasksRoutes);
+app.use(imgRoutes);
 
-// ruta
-app.use('/api', ImgRoutes);
 
-// esta carpeta sera usada para almacenar archivos publicos
-app.use('/images', express.static(path.resolve('public')));
+app.set('views', path.join(__dirname, 'views')); // indicar donde esta la carpeta 
+app.set('view engine', 'ejs'); // motor de platillas
+
+// middlewares
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/images'),
+    filename: (req, file, cb) => { 
+        //  cb(null, file.originalname); ----> guarde los archivos con el nombre original
+        cb(null, uuidv4() + path.extname(file.originalname) // guarde la img con id aleatorio y su extension
+        .toLocaleLowerCase()); //pase a minusculas
+    }
+});
+
+app.use(multer({
+    storage,
+    dest: path.join(__dirname, 'public/images'),
+    limits: {fileSize: 1000000}, //tamaño de la img MB 
+    fileFilter: (req, file, cb) => {
+        const filetypes = /jpeg|jpg|png|gif/; //Tipo de img q quiero
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(path.extname(file.originalname));
+
+        if (mimetype && extname) {
+            return cb(null, true);
+        }
+
+        cb("Error: El archivo debe ser una img valida");
+    }
+}).single('image'));
+
+// archivos estaticos  (para q la carpeta pueda ser accedida desde el navegador)
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 export default app;
--- src/routes/img.routes.js
+++ src/routes/img.routes.js
@@ -1,18 +1,28 @@
 import { Router } from "express";
-import {
-  crearPhoto,
-  getPhotos,
-  getPhotoId,
-  updatePhoto,
-  deletePhoto,
-} from "../controllers/photo.controller.js";
-import multer from "../libs/multer.js";
+// import {
+//   crearPhoto,
+//   getPhotos,
+//   getPhotoId,
+//   updatePhoto,
+//   deletePhoto,
+// } from "../controllers/photo.controller.js";
+// import multer from "../libs/multer.js";
 
 const router = Router();
 
-router.route("/photos").post(multer.single("image"), crearPhoto).get(getPhotos);
-router.route("/photos/:id", getPhotoId)
-.put(updatePhoto)
-.delete(deletePhoto);
+//
+router.get('/', (req, res) => {
+    res.render('index.ejs')
+});
+
+router.post('/upload', (req, res) => {
+    console.log(req.file);
+    res.send('uploaded file')
+});
+
+// router.route("/photos").post(multer.single("image"), crearPhoto).get(getPhotos);
+// router.route("/photos/:id", getPhotoId)
+// .put(updatePhoto)
+// .delete(deletePhoto);
 
 export default router;
